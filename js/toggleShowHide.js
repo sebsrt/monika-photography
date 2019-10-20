8e1570de9841c6e3b96eefcf70808d813f0c1e7a
srobaszkiewicz <srobaszkiewicz@spyro-soft.com>
2019-10-20 21:48:34 +0200

    Zmiana tekstu follow us on instagram Dodana funkcja toggleShowHide.js

---
+++ js/toggleShowHide.js
@@ -0,0 +1,8 @@
+function showOrHide() {
+    var x = document.getElementById("morePhotos");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
