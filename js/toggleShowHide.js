f179a525baa0fa1c455320f3ad756fe2021c6946
Sebastian Robaszkiewicz <srobaszkiewicz@spyro-soft.com>
2019-10-23 19:58:26 +0200

    Pricelist completed

--- js/toggleShowHide.js
+++ js/toggleShowHide.js
@@ -1,7 +1,9 @@
 function showOrHide() {
     var x = document.getElementById("morePhotos");
+    var button = document.getElementById("morePhotosButton");
     if (x.style.display === "none") {
         x.style.display = "block";
+        button.style.display = "none";
     } else {
         x.style.display = "none";
     }
